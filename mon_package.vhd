11f247084bb46893a9e45089f6ab73f7413f5834
tarek TALSI <141179083+Tarek-TALSI@users.noreply.github.com>
2025-10-07 11:09:14 +0200

    Update mon_package.vhd

--- mon_package.vhd
+++ mon_package.vhd
@@ -5,28 +5,28 @@ use ieee.std_logic_1164.all ;
 use ieee.std_logic_unsigned.all ;
 
 package mes_fonctions is
-function cpl2 (entree: std_logic_vector; N: natural) 
-					return std_logic_vector;
-end mes_fonctions;
+function cpl2 (entree : std_logic_vector ; N : natural) 
+					return std_logic_vector ;
+end mes_fonctions ;
 -------------------------------------------------------------------------
 package body mes_fonctions is
-function cpl2 (entree: std_logic_vector; N: natural) 
-					return std_logic_vector is
+function cpl2 (entree : std_logic_vector ; N : natural) 
+					return std_logic_vector  is
  
-variable temps: std_logic_vector( N-1 downto 0);
+variable temp : std_logic_vector( N-1 downto 0) ;
 begin
-temp:= not entree;
-temp:= temp + '1';
-return temp;
-end cpl2;
-end mes_fonctions;
+temp := not entree;
+temp := temp + '1' ;
+return temp ;
+end cpl2 ;
+end mes_fonctions ;
 --------------------------------------------------------------------------
 library ieee ;
 use ieee.std_logic_1164.all ;
 
 package CHECK_PKG is
 
--- D�claration de la proc�dures check_setup. (pre positionnement)
+-- Déclaration de la procédures check_setup. (pre positionnement)
 procedure check_setup(
 		signal clk 		: in std_logic;
 		signal din 		: in std_logic_vector;
@@ -34,7 +34,7 @@ procedure check_setup(
 			severite	: in severity_level:= warning;
 			hdeb 		: in time := time'low;
 			hfin		: in time := time'high );
--- D�claration de la proc�dures T_hold. (maintien)
+-- Déclaration de la procédures T_hold. (maintien)
 procedure check_hold(
 		signal clk 		: in std_logic;
 		signal din 		: in std_logic_vector;
@@ -49,8 +49,8 @@ library ieee ;
 use ieee.std_logic_1164.all ;
 
 package body CHECK_PKG is		
--- Sp�cification du corps du paquetage.
---la proc�dure check_setup
+-- Spécification du corps du paquetage.
+--la procédure check_setup
 procedure check_setup(
 	signal clk 		: in std_logic;
 		signal din 	: in std_logic_vector;
@@ -65,7 +65,7 @@ If clk = '1' and clk'event then
 --wait until clk = 'l' and clk'event;
 	if now >= hdeb and now <= hfin then
 	assert din'last_event >= t_setup 
-	report "temps de setup non respect�" severity severite;
+	report "temps de setup non respecté" severity severite;
 	elsif now > hfin then
 	wait;
 	end if;
@@ -88,7 +88,7 @@ begin
         
         wait for t_hold;
         assert din'last_event >= t_hold
-          report "T_hold non respect�" severity severite;
+          report "T_hold non respecté" severity severite;
       elsif now > hfin then
         wait;
       end if;
@@ -98,5 +98,6 @@ end check_hold;
 
 End CHECK_PKG;
 --------------------------------------------------------------------------
+
 
 
